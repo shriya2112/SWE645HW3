35a3b846dd639cd84fe5611a20eb465b610e947f
shriya2112 <shriyaatluri2112@gmail.com>
2024-11-24 14:53:58 -0500

    Update SurveyController.java

--- src/main/java/com/example/controller/SurveyController.java
+++ src/main/java/com/example/controller/SurveyController.java
@@ -18,7 +18,7 @@ public class SurveyController {
     }
 
     // Root endpoint for the application
-	@GetMapping("/")
+	@GetMapping("/student")
 	public String home() {
 		return "Welcome to the Student Management Application! Please use this link to view database_1 which has survey table.";
 	}
